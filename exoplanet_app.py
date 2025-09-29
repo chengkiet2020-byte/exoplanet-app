63f4ba67282c1f643224355db514c681e9d6c169
chengkiet2020-byte <chengkiet2020@gmail.com>
2025-09-29 21:17:27 +0800

    Update exoplanet_app.py

--- exoplanet_app.py
+++ exoplanet_app.py
@@ -212,6 +212,7 @@ if page == "Home":
     col1, col2 = st.columns([2, 1])
 
     with col1:
+        st.markdown("<div style='margin-left:300px'></div>", unsafe_allow_html=True)
         st.image("https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg", width=220)  
     
     with col2: 
@@ -795,6 +796,7 @@ elif page == "Researcher Mode":
         """,
         unsafe_allow_html=True
     )
+
 
 
 
