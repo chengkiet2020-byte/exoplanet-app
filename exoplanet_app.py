14b5b8416beac6b1744dd554860abc9e7c3a32d6
chengkiet2020-byte <chengkiet2020@gmail.com>
2025-09-29 19:59:11 +0800

    Update exoplanet_app.py

--- exoplanet_app.py
+++ exoplanet_app.py
@@ -191,13 +191,13 @@ st.markdown(
 
 # --- Home Page ---
 if page == "Home":
-    col1, col2, col3 = st.columns([1, 2, 1])
+    col1, col2, col3 = st.columns([1, 0.5, 1.5])
 
     with col1:
         st.image("https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg", width=220)  
     
     with col2: 
-        st.image("https://imgur.com/a/wgdyC9e")
+        st.image("https://i.imgur.com/TL2RXUL.png"， width=250)
     # st.title("🚀 Exoscan 117")
     st.markdown(
     """
@@ -716,6 +716,7 @@ elif page == "Researcher Mode":
         """,
         unsafe_allow_html=True
     )
+
 
 
 
