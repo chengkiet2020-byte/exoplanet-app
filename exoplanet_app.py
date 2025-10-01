156386eb44d5b91ef484bd2641deb5d92d9145cc
chengkiet2020-byte <chengkiet2020@gmail.com>
2025-10-01 21:00:25 +0800

    Update exoplanet_app.py

--- exoplanet_app.py
+++ exoplanet_app.py
@@ -476,7 +476,7 @@ if page == "Home":
     """
     st.markdown(page_bg_video, unsafe_allow_html=True)
 
-     st.markdown(
+    st.markdown(
     "<h1 style='text-align: center; font-size: 90px; color: #00BFFF;'>"Team: 🌌 Galactic Explorer 117</h1>",
     unsafe_allow_html=True
     )
@@ -1000,6 +1000,7 @@ elif page == "Researcher Mode":
         unsafe_allow_html=True
 
     )
+
 
 
 
