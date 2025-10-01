25701deacec7f2d0c2c6ad8dfe71e15e31cdf7bf
chengkiet2020-byte <chengkiet2020@gmail.com>
2025-10-01 17:41:45 +0800

    Update exoplanet_app.py

--- exoplanet_app.py
+++ exoplanet_app.py
@@ -355,7 +355,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 # logo 图标
-    col_empty, col_left, col_mid, col_right, col_empty2 = st.columns([1.2, 0.8, 1.5, 1, 1])
+col_empty, col_left, col_mid, col_right, col_empty2 = st.columns([1.2, 0.8, 1.5, 1, 1])
 
     with col_left:
         st.markdown("<div style='margin-top:-20px'></div>", unsafe_allow_html=True)
@@ -858,6 +858,7 @@ elif page == "Researcher Mode":
         """,
         unsafe_allow_html=True
     )
+
 
 
 
